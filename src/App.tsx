69a935e1a5116047c56b97fb3388e51f653ff41b
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2023-11-22 05:30:33 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button>Simeon Ayendi</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <p>This is simeon ayendi</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
