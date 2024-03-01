af0d93bbbb024754c8f68ffb7ecd95e11c0631d9
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2024-02-29 23:56:56 -0800

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,35 +4,7 @@ import './header.css';
 const Header = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg ">
-                
-            <div className="navbar-brand">
-                    <a href="#" id="home">
-                        Ayendi Simeon.
-                        </a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-            </div>
-                
-                <div className="navbar-nav collapse navbar-collapse " id='navbarNav'>
-                    <ul className=" list-inline justify-content-end"  id='navbarNav'>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Skills</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Contact</a>
-                        </li>
-                    </ul>
-                    
-                </div>
-            </nav>
+      
         </div>
     )
 }
