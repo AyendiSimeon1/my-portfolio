1a895a3c0136ab4c0acd2ed016ed0026cdee4c38
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2024-03-03 09:08:04 -0800

    Update product.tsx

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -6,7 +6,10 @@ const Product = () => {
 
     return (
         <div className='text-center'>
-
+           <div clasName="border-solid text-white text-2xl p-2 mx-4">
+              <img src={} h={} w={} />
+     
+           </div>
                 
         </div>
     )
