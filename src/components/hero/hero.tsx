1cd1f56489f41f461807beff5679047e2c98bedd
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2024-03-03 09:03:46 -0800

    Update hero.tsx

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,8 +4,15 @@ import  hero2  from '../../assets/hero-2.jpg';
 
 const Hero = () => {
     return (
-        <div className="container">
-           
+        <div className="text-white-100">
+           <div class name"text-white-100 text-2xl">
+   Hi 👋 I am Ayendi Simeon        
+</div>
+<div class name"text-white-100 text-2xl">
+  Backend Developer        
+</div>
+<button class name="rounded-full px-2 border-3">Download Resume</button>
+
 </div>
                           
     )
