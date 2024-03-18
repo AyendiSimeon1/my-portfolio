7d9675846975507b7e7684f9559e6070b8fc633e
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2024-03-17 17:31:19 -0700

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,16 @@ const Header = () => {
     return (
         <div>
             <header>
-            
+            <nav class="bg-gray-800 text-white px-4 py-2 flex justify-between">
+  <a href="#" class="text-xl font-bold">My Website</a>
+  
+  <ul class="flex space-x-4">
+    <li><a href="#" class="hover:text-gray-200">Home</a></li>
+    <li><a href="#" class="hover:text-gray-200">About</a></li>
+    <li><a href="#" class="hover:text-gray-200">Contact</a></li>
+  </ul>
+</nav>
+
             </header>
       
         </div>
